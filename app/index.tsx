523422db2c37f13cbfcd76d51be47dba607c8ff2
Leonardo E. Dominguez <focuux@gmail.com>
2025-11-02 15:59:07 -0400

    feat: add glass effect repro

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,8 @@
+import { GlassView } from "expo-glass-effect";
 import { Text, View } from "react-native";
+import { withUniwind } from "uniwind";
+
+const UniwindGlassView = withUniwind(GlassView);
 
 export default function Index() {
   return (
@@ -13,6 +17,14 @@ export default function Index() {
         <Text>Edit app/index.tsx to edit this screen.</Text>
         <View className="w-hairline h-4 bg-blue-500" />
       </View>
+      <Text className="my-4">With Uniwind</Text>
+      <UniwindGlassView className="p-4 rounded-full">
+        <Text>Hello World</Text>
+      </UniwindGlassView>
+      <Text className="my-4">With Style</Text>
+      <UniwindGlassView style={{ padding: 16, borderRadius: 9999 }}>
+        <Text>Hello World</Text>
+      </UniwindGlassView>
     </View>
   );
 }
