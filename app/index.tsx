283c0c7a5fcb36d18a87a7afa662a60b5bf56f5f
Leonardo E. Dominguez <focuux@gmail.com>
2025-11-02 15:45:03 -0400

    upgrade to canary

--- app/index.tsx
+++ app/index.tsx
@@ -9,8 +9,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <View className="bg-red-500 rounded-xl">
+      <View className="bg-red-500 rounded-full p-4">
         <Text>Edit app/index.tsx to edit this screen.</Text>
+        <View className="w-hairline h-4 bg-blue-500" />
       </View>
     </View>
   );
